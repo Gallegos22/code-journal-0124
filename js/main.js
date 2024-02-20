3b512cc4666dfe3ca6087b5712bc7ed1e7c83e10
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2024-02-20 00:12:51 +0000

    Add an 'input' event listener to the Photo URL input that sets the src attribute of the photo preview to the input's value when the user types or pastes in a new URL. #5

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
+'use strict';
+/* global data */
 const $photoURL = document.querySelector('#photo-url');
 console.log($photoURL);
-
 if (!$photoURL) throw new Error('The $photoURL query failed');
-
 const $image = document.querySelector('img');
 console.log($image);
-
 if (!$image) throw new Error('The $image query failed');
-
-$photoURL?.addEventListener('input', (event: Event) => {
+$photoURL?.addEventListener('input', (event) => {
   console.log('event.target:', event.target);
-
-  const $eventTarget = event.target as HTMLInputElement;
+  const $eventTarget = event.target;
   $image.src = $eventTarget.value;
 });
