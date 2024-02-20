8e63558a7df20202332fac05184221501b99c9a7
Eduardo Gallegos <gallegoseduardo414@gmail.com>
2024-02-20 19:03:06 +0000

    Add a 'submit' event listener to the form element that: #6

--- ts/data.ts
+++ ts/data.ts
@@ -1,8 +1,16 @@
 /* exported data */
+interface Data {
+  view: 'entries' | 'entry-form';
+  entries: Entry[];
+  editing: null | Entry;
+  nextEntryId: number;
+}
 
-let data = {
+const data: Data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1,
 };
+
+console.log(data);
--- ts/main.ts
+++ ts/main.ts
@@ -1,16 +1,57 @@
+interface Entry {
+  entryId: number;
+  title: string;
+  imageUrl: string;
+  notes: string;
+}
+
 const $photoURL = document.querySelector('#photo-url');
-console.log($photoURL);
 
 if (!$photoURL) throw new Error('The $photoURL query failed');
 
 const $image = document.querySelector('img');
-console.log($image);
 
 if (!$image) throw new Error('The $image query failed');
 
 $photoURL?.addEventListener('input', (event: Event) => {
-  console.log('event.target:', event.target);
-
   const $eventTarget = event.target as HTMLInputElement;
   $image.src = $eventTarget.value;
+});
+
+const $form = document.querySelector('#form') as HTMLFormElement;
+
+if (!$form) throw new Error('The $form query failed');
+
+const $title = document.querySelector('#title-box') as HTMLFormElement;
+
+if (!$title) throw new Error('#title-box');
+
+const $notes = document.querySelector('#notes') as HTMLFormElement;
+
+if (!$notes) throw new Error('The $notes query failed');
+
+$form.addEventListener('submit', (event: Event) => {
+  event.preventDefault();
+
+  const $form = document.querySelector('#form') as HTMLFormElement;
+
+  const newData: Entry = {
+    title: $form.$title.value,
+    imageUrl: $form.$image.src,
+    notes: $form.$notes.value,
+    entryId: $form.data.nextEntryId,
+  };
+
+  data.nextEntryId++;
+  data.entries.unshift(newData);
+
+  const image = document.getElementById('placeholder-img') as HTMLFormElement;
+  image.src = '/images/placeholder-image-square.jpg';
+
+  if (!image) throw new Error('The image query failed');
+
+  const form = document.querySelector('#myForm') as HTMLFormElement;
+
+  if (!form) throw new Error('The form query failed');
+  form.reset();
 });
